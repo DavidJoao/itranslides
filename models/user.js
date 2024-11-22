afc4838d5fd7fe67319687ca62172c056acb441b
DavidJoao <davidsandoval596@gmail.com>
2024-11-21 21:49:39 -0800

    CREATED User model and export

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
-// const mongoose = require("../src/app/lib/mongoConnection")
+const mongoose = require("../src/app/lib/mongoConnection")
 
-// const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
+    nickname: String,
+    presentations: Array,
+})
 
-// })
+module.exports = mongoose.models.User || mongoose.model("User", userSchema)
