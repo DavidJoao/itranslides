d72e22e8af3d468ff255060269f2f68fd5eca028
DavidJoao <davidsandoval596@gmail.com>
2024-11-21 19:28:24 -0800

    EDITED Main Screen

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
-import Image from "next/image";
+'use client'
+import axios from 'axios'
 
 export default function Home() {
-  return (
-    <div className="">
 
+  const handleHello = (e) => {
+    e.preventDefault()
+    axios.get("/pages/api/helloworld/").then(res => console.log(res))
+  }
+  return (
+    <div className="bg-black w-full h-screen text-white flex flex-col items-center justify-center">
+      <h1 className="font-bold text-[50px]">ITranslide</h1>
+      <h2 className="font-semibold text-[20px]">The Ideal Website For Your Presentations</h2>
+      <form className="flex flex-col items-center justify-center gap-3 mt-4">
+        <label>Nickname:</label>
+        <input required placeholder="SpongeBob" className="input w-[300px]"/>
+        <button type="submit" className="button w-[300px]" onClick={handleHello}>Enter</button>
+      </form>
     </div>
   );
 }
