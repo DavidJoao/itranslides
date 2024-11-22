adcb9ac8d965679dfd10a105c37ef935b56fbc44
DavidJoao <davidsandoval596@gmail.com>
2024-11-21 21:50:02 -0800

    REMOVED NextAuth wrapper as we won't need auth

--- src/app/components/ContextProvider.js
+++ src/app/components/ContextProvider.js
@@ -1,19 +1,14 @@
 "use client"
-import { SessionProvider } from 'next-auth/react'
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState } from "react"
 
-const AppContext = createContext();
+const AppContext = createContext()
 
-const Provider = ( { children } ) => {
-    const [activeUser, setActiveUser] = useState(null)
+const Provider = ({ children }) => {
+	const [activeUser, setActiveUser] = useState(null)
 
-    return (
-        <SessionProvider>
-            <AppContext.Provider value={{ activeUser, setActiveUser }}>
-                {children}
-            </AppContext.Provider>
-        </SessionProvider>
-    )
+	return (
+		<AppContext.Provider value={{ activeUser, setActiveUser }}>{children}</AppContext.Provider>
+	)
 }
 
 export default Provider
