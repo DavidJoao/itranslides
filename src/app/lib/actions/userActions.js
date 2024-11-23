626948c3dd442d076080b7f15a8edc2b51342df2
DavidJoao <davidsandoval596@gmail.com>
2024-11-22 23:47:20 -0800

    CREATE Functions for actions: checkSession and getAndSetSession

--- src/app/lib/actions/userActions.js
+++ src/app/lib/actions/userActions.js
@@ -7,5 +7,32 @@ export const createUser = async (user) => {
     } catch (error) {
         return error
     }
+}
 
+export const checkSession = async () => {
+    try {
+        const response = await axios.get("/pages/api/user/session");
+        return response;
+    } catch (error) {
+        return error;
+    }
+}
+
+export const getAndSetSession = async () => {
+    try {
+        const session = await checkSession();
+        if (session?.status === 200) {
+            const userId = session?.data?.userId?.value;
+            if (userId) {
+                const res = await axios.get(`/pages/api/user?userId=${userId}`, {
+                    headers: { 'Content-Type': 'application/json' }
+                });
+                return res.data;
+            } else {
+                console.log("No userId found in session.");
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
