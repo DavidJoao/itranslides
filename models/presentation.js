cc67a4fa1be8f043ceb5412878e7d4992e17237c
DavidJoao <davidsandoval596@gmail.com>
2024-11-22 23:46:47 -0800

    CREATED Model for presentation

--- models/presentation.js
+++ models/presentation.js
@@ -1,7 +1,28 @@
 const mongoose = require("../src/app/lib/mongoConnection")
 
-const presentationSchema = new mongoose.Schema({
+const ObjectVersionSchema = new mongoose.Schema(
+	{
+		timestamp: { type: Date, default: Date.now },
+		objects: [{ type: mongoose.Schema.Types.Mixed }],
+		description: String,
+	},
+	{ _id: false }
+)
 
+const PresentationSchema = new mongoose.Schema({
+    name: String,
+	creationDate: { type: Date, default: Date.now },
+	versions: [ObjectVersionSchema],
+	creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+	viewers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+	editors: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 })
 
-module.exports = mongoose.models.Presentation || mongoose.model("Presentation", presentationSchema)
+PresentationSchema.virtual("currentObjects").get(function () {
+	if (this.versions.length > 0) {
+		return this.versions[this.versions.length - 1].objects
+	}
+	return []
+})
+
+module.exports = mongoose.models.Presentation || mongoose.model("Presentation", PresentationSchema)
