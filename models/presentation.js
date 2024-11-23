3acda8dcb9d7dbbedf0de3dc2e8c3847dee6a895
DavidJoao <davidsandoval596@gmail.com>
2024-11-22 21:38:15 -0800

    ENABLED Schema for presentation

--- models/presentation.js
+++ models/presentation.js
@@ -1,5 +1,7 @@
-// const mongoose = require("../src/app/lib/mongoConnection")
+const mongoose = require("../src/app/lib/mongoConnection")
 
-// const presentationSchema = new mongoose.Schema({
+const presentationSchema = new mongoose.Schema({
 
-// })
+})
+
+module.exports = mongoose.models.Presentation || mongoose.model("Presentation", presentationSchema)
