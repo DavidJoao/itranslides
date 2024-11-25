0d6f345a9d3cd7cb9f79363efe97d20c33d63a72
DavidJoao <davidsandoval596@gmail.com>
2024-11-25 15:42:01 -0800

    CREATED Main Layout for the presentation page

--- src/app/pages/presentation/[presentationId]/page.js
+++ src/app/pages/presentation/[presentationId]/page.js
@@ -1,29 +1,73 @@
 'use client'
-
-import { getPresentationById } from '@/app/lib/actions/presentationActions';
+import { createSlide, deleteSlide, getPresentationById } from '@/app/lib/actions/presentationActions';
 import React, { useEffect, useState } from 'react'
+import Slide from '@/app/components/Slide';
+import { icons } from '@/app/lib/icons';
 
-const PresentationPage = ({ params }) => {
+    const PresentationPage = ({ params }) => {
 
-  const [presentationId, setPresentationId] = useState("")
+    const [presentationId, setPresentationId] = useState("")
+    const [presentation, setPresentation] = useState({})
+    const [currentSlide, setCurrentSlide] = useState()
 
-  const getParams = async () => {
-    const { presentationId } = await params;
-    setPresentationId(presentationId)
-    return presentationId
-  }
-    useEffect(() => {
-      const fetchPresentation = async () => {
-        const id = await getParams();
-        const res = await getPresentationById(id)
-        console.log(res?.data?.presentation)
-        return res;
-      }
-      fetchPresentation()
-    }, [])
-  return (
-    <div>{presentationId}</div>
-  )
-}
+    const getParams = async () => {
+        const { presentationId } = await params;
+        setPresentationId(presentationId)
+        return presentationId
+    }
+        useEffect(() => {
+            fetchPresentation()
+        }, [])
 
-export default PresentationPage
+        const fetchPresentation = async () => {
+            const id = await getParams();
+            const res = await getPresentationById(id)
+            setPresentation(res?.data?.presentation)
+            console.log(res?.data?.presentation)
+            return res;
+        }
+
+        const handleSlideAddition = async (e) => {
+            e.preventDefault();
+            await createSlide(presentationId)
+            await fetchPresentation()
+        }
+
+
+    return (
+        <div className='flex flex-col md:flex-row w-screen h-screen'>
+            <div className='w-full md:w-[20%] h-[20%] md:h-full p-5 bg-slate-600 text-white'>
+                { presentation?.slides?.length === 0 ? (
+                    <div className='flex flex-col items-center justify-center p-1'>
+                        <p>Empty Presentation</p>
+                        <button className='button w-[150px] text-white font-bold' onClick={handleSlideAddition}>Add Slide</button>
+                    </div>
+                ) : (
+                    <div className='h-full flex flex-row md:flex-col items-center justify-start p-1 gap-2 overflow-scroll'>
+                        { presentation?.slides?.map((slide, index) => {
+                            return (
+                            <div key={index} className='slide flex-shrink-0 text-black flex flex-row items-start justify-between p-2'>
+                                <p>{index + 1}</p>
+                                <button className='red-button text-white' onClick={async (e) => {
+                                    e.preventDefault();
+                                    await deleteSlide(presentationId, slide._id);
+                                    await fetchPresentation();
+                                    }}>{icons.trash}</button>
+                            </div>    
+                            )
+                        }) }
+                        <button className='button w-[150px] text-white font-bold' onClick={handleSlideAddition}>Add Slide</button>
+                    </div>
+                ) }
+            </div>
+            <div className='border-[1px] border-black w-full md:w-[80%] h-full flex items-center justify-center p-5 bg-slate-400'>
+                <Slide />
+            </div>
+            <div className='border-[1px] border hidden'>
+                {/* FLOATING TOOLS DIV */}
+            </div>
+        </div>
+    )
+    }
+
+    export default PresentationPage
