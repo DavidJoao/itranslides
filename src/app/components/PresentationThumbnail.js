49673677bbaec37d84795f915d3a6d74b178c238
DavidJoao <davidsandoval596@gmail.com>
2024-11-24 18:45:08 -0800

    ADDED HandleDelete to presentations created by logged in user

--- src/app/components/PresentationThumbnail.js
+++ src/app/components/PresentationThumbnail.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { navigate } from '../lib/redirect'
 
-const PresentationThumbnail = ( {index, presentation} ) => {
+const PresentationThumbnail = ( {index, presentation, handleDelete} ) => {
   return (
     <div className='flex flex-col items-center justify-center'>
       <div className='presentation-thumbnail'>
 
       </div>
-      <p>{presentation.name}</p>
+      <div className='flex flex-col items-center gap-2'>
+        <p>{presentation.name}</p>
+        <button className='button text-white w-[100px]' onClick={() => navigate(`/pages/presentation/${presentation._id}`)}>Join</button>
+        <button className='red-button text-white w-[100px]' onClick={(e) => handleDelete(e, presentation)}>Delete</button>
+      </div>
     </div>
   )
 }
