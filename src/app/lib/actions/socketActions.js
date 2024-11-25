d819bc4e32e3132e2d1541cf674be8682b3476b5
DavidJoao <davidsandoval596@gmail.com>
2024-11-24 18:46:33 -0800

    CREATED socketActions, emitNewPresentation, emitDeletePresentation

--- src/app/lib/actions/socketActions.js
+++ src/app/lib/actions/socketActions.js
@@ -1,22 +1,10 @@
 import io from 'socket.io-client'
 const socket = io('http://localhost:3001');
 
-export const emitNewPresentation = async (user, presentationName, fetchPresentations) => {
-    socket.emit("new presentation", presentationName)
-    socket.on("New Presentation", async () => {
-        await fetchPresentations(user)
-    })
-    return () => {
-        socket.off("New Presentation")
-    }
+export const emitNewPresentation = async (presentationName) => {
+    socket.emit("new presentation", presentationName);
 }
 
 export const emitDeletePresentation = async (user, presentationName, fetchPresentations) => {
-    socket.emit("delete presentation", presentationName)
-    socket.on("delete presentation", async () => {
-        await fetchPresentations(user)
-    })
-    return () => {
-        socket.off("Deleted Presentation")
-    }
+    socket.emit("delete presentation", presentationName);
 }
