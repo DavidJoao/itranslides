7a5ea146ac9e3dc6703a2692b081650d10ee500b
DavidJoao <davidsandoval596@gmail.com>
2024-11-26 21:25:38 -0800

    CHANGED PresentationThumbnail: Setting presentation variable when user joins

--- src/app/components/PresentationThumbnail.js
+++ src/app/components/PresentationThumbnail.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { navigate } from '../lib/redirect'
+import { useAppContext } from './ContextProvider'
+import { getPresentationById } from '../lib/actions/presentationActions'
 
 const PresentationThumbnail = ( {index, presentation, handleDelete} ) => {
+
+  const { setPresentation } = useAppContext()
+
+  const handleSelectPresentation = async () => {
+    const res = await getPresentationById(presentation?._id);
+    await setPresentation(res?.data?.presentation);
+    await navigate(`/pages/presentation/${presentation._id}`)
+};
+
   return (
     <div className='flex flex-col items-center justify-center'>
       <div className='presentation-thumbnail'>
@@ -9,7 +20,7 @@ const PresentationThumbnail = ( {index, presentation, handleDelete} ) => {
       </div>
       <div className='flex flex-col items-center gap-2'>
         <p>{presentation.name}</p>
-        <button className='button text-white w-[100px]' onClick={() => navigate(`/pages/presentation/${presentation._id}`)}>Join</button>
+        <button className='button text-white w-[100px]' onClick={handleSelectPresentation}>Join</button>
         <button className='red-button text-white w-[100px]' onClick={(e) => handleDelete(e, presentation)}>Delete</button>
       </div>
     </div>
