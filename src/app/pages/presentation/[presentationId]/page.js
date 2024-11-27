5d64898961bafd2de8a7a4f2624a925bdb3bdb8c
DavidJoao <davidsandoval596@gmail.com>
2024-11-26 23:40:43 -0800

    DELETION: Got Rid of most of content for readibility, now separated everything into components and global state

--- src/app/pages/presentation/[presentationId]/page.js
+++ src/app/pages/presentation/[presentationId]/page.js
@@ -1,103 +1,70 @@
 'use client'
-import { createSlide, deleteSlide, getPresentationById } from '@/app/lib/actions/presentationActions';
-import React, { useEffect, useState } from 'react'
-import Slide from '@/app/components/Slide';
-import { icons } from '@/app/lib/icons';
-import io from 'socket.io-client'
-import { emitDeleteSlide, emitNewSlide } from '@/app/lib/actions/socketActions';
+import Canvas from '@/app/components/Canvas';
+import { useAppContext } from '@/app/components/ContextProvider';
+import Slides from '@/app/components/Slides';
+import { createSlide, getPresentationById } from '@/app/lib/actions/presentationActions';
+import { useEffect } from 'react';
+import Menu from '@/app/components/Menu';
+import { emitNewSlide } from '@/app/lib/actions/socketActions';
 
 const PresentationPage = ({ params }) => {
 
-    const socket = io('http://localhost:3001');
-        
-    const [presentationId, setPresentationId] = useState("")
-    const [presentation, setPresentation] = useState({})
-    const [currentSlide, setCurrentSlide] = useState()
+    const { presentation, setPresentation } = useAppContext();
 
-    const getParams = async () => {
+    const initiatePresentation = async () => {
         const { presentationId } = await params;
-        setPresentationId(presentationId)
-        return presentationId
+        const res = await getPresentationById(presentationId);
+        await setPresentation(res?.data?.presentation);
+        return presentation
     }
 
     useEffect(() => {
-		const handleSocketCreation = async () => {
-			socket.on("New Slide", async () => {
-				await fetchPresentation(presentationId)
-			})
-		}
-		handleSocketCreation()
-		return () => {
-			socket.off("New Slide")
-		}
-	}, [presentation])
+        initiatePresentation();
+    }, [])
 
-    useEffect(() => {
-		const handleSocketSlideDeletion = async () => {
-			socket.on("Delete Slide", async () => {
-				await fetchPresentation(presentationId)
-			})
-		}
-		handleSocketSlideDeletion()
-		return () => {
-			socket.off("Delete Slide")
-		}
-	}, [presentation])
-
-	useEffect(() => {
-		fetchPresentation()
-	}, [])
-
-	const fetchPresentation = async () => {
-		const id = await getParams()
-		const res = await getPresentationById(id)
-		setPresentation(res?.data?.presentation)
-		console.log(res?.data?.presentation)
-		return res
-	}
-
-	const handleSlideAddition = async e => {
-		e.preventDefault()
-		await createSlide(presentationId)
+    const handleSlideCreation = async () => {
+        await createSlide(presentation?._id)
+        const res = await getPresentationById(presentation?._id)
+        await setPresentation(res?.data?.presentation)
         await emitNewSlide()
-		await fetchPresentation()
-	}
-
+    }
 
     return (
-        <div className='flex flex-col md:flex-row w-screen h-screen'>
-            <div className='w-full md:w-[20%] h-[20%] md:h-full p-5 bg-slate-600 text-white'>
-                { presentation?.slides?.length === 0 ? (
-                    <div className='flex flex-col items-center justify-center p-1'>
-                        <p>Empty Presentation</p>
-                        <button className='button w-[150px] text-white font-bold' onClick={handleSlideAddition}>Add Slide</button>
-                    </div>
-                ) : (
-                    <div className='h-full flex flex-row md:flex-col items-center justify-start p-1 gap-2 overflow-scroll'>
-                        { presentation?.slides?.map((slide, index) => {
-                            return (
-                            <div key={index} className='slide flex-shrink-0 text-black flex flex-row items-start justify-between p-2'>
-                                <p>{index + 1}</p>
-                                <button className='red-button text-white' onClick={async (e) => {
-                                    e.preventDefault();
-                                    await deleteSlide(presentationId, slide._id);
-                                    await emitDeleteSlide();
-                                    await fetchPresentation();
-                                    }}>{icons.trash}</button>
-                            </div>    
-                            )
-                        }) }
-                        <button className='button w-[150px] text-white font-bold' onClick={handleSlideAddition}>Add Slide</button>
-                    </div>
-                ) }
+        <>
+        {presentation ? (
+            <div className='flex flex-col md:flex-row w-screen h-screen'>
+                <div className='w-full md:w-[20%] h-[20%] md:h-full p-5 bg-slate-600 text-white'>
+                    { presentation?.slides?.length === 0 ? (
+                        <div className='flex flex-col items-center justify-center p-1'>
+                            <p>Empty Presentation</p>
+                            <button className='button w-[150px] text-white font-bold'  onClick={handleSlideCreation}>Add Slide</button>
+                        </div>
+                    ) : (
+                        <div className='h-full flex flex-row md:flex-col items-center justify-start p-1 gap-2 overflow-scroll'>
+                            <Slides />
+                            <button className='button w-[150px] text-white font-bold' onClick={handleSlideCreation}>Add Slide</button>
+                        </div>
+                    ) }
+                </div>
+
+
+                <div className='border-[1px] border-black w-full md:w-[80%] h-full flex items-center justify-center p-5 bg-slate-400'>
+                    <Canvas />
+                </div>
+
+
+                <div className='fixed bottom-5 left-0 right-0 flex flex-col items-center justify-center'>
+                    <Menu />
+                </div>
+
+
             </div>
-            <div className='border-[1px] border-black w-full md:w-[80%] h-full flex items-center justify-center p-5 bg-slate-400'>
-                <Slide />
+        ) : (
+            <div className='flex flex-col md:flex-row w-screen h-screen'>
+                <p>Loading Presentation...</p>
             </div>
-            <div className='border-[1px] border hidden'>
-                {/* FLOATING TOOLS DIV */}
-            </div>
-        </div>
+        )}
+        </>
     )
     }
 
