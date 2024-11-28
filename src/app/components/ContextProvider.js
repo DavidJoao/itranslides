20db746cbf241f37d1f5d5c2191139fdb7d7e038
DavidJoao <davidsandoval596@gmail.com>
2024-11-28 14:50:07 -0800

    ADDED: Function to check if user is viewer, call to update roles in useEffect

--- src/app/components/ContextProvider.js
+++ src/app/components/ContextProvider.js
@@ -2,20 +2,35 @@
 import { createContext, useContext, useState, useEffect } from "react"
 import { getPresentationById } from "../lib/actions/presentationActions"
 import { socket } from "../lib/socket"
+import { getAndSetSession, hasRole } from "../lib/actions/userActions"
 
 const AppContext = createContext()
 
 const Provider = ({ children }) => {
-	
-	const [activeUser, setActiveUser] = useState(null)
+    
+    const [activeUser, setActiveUser] = useState(null)
 	const [presentation, setPresentation] = useState(null)
 	const [currentSlide, setCurrentSlide] = useState(presentation?.slides[0] || null)
     const [connectedUsers, setConnectedUsers] = useState([]);
+    const [viewers, setViewers] = useState([])
+    const [editors, setEditors] = useState([])
 
 	const setThePresentation = async () => {
 		const res = await getPresentationById(presentation?._id);
 		setPresentation(res?.data?.presentation);
 	}
+
+    const isUserViewer = (presentation, activeUser) => {
+        return presentation?.viewers.some(viewerId => viewerId === activeUser?._id);
+    };
+
+    useEffect(() => {
+        const setUser = async () => {
+            const response = await getAndSetSession()
+            setActiveUser(response?.user)
+        } 
+        setUser()
+    }, [])
 
 	useEffect(() => {
 		if (presentation?.slides?.length) {
@@ -34,13 +49,20 @@ const Provider = ({ children }) => {
             const updatePresentationHandler = async () => {
                 await setThePresentation();
             };
-    
+
+            socket.on("Update Roles", (roleData) => {
+                setEditors(roleData?.editors);
+                setViewers(roleData?.viewers);
+                console.log(roleData)
+            });
+
             socket.on("update users", updateUsersHandler);
             socket.on("New Slide", updatePresentationHandler);
             socket.on("Delete Slide", updatePresentationHandler);
             socket.on("Slide Change", updatePresentationHandler);
     
             return () => {
+                socket.off("update roles", updatePresentationHandler)
                 socket.off("update users", updateUsersHandler);
                 socket.off("New Slide", updatePresentationHandler);
                 socket.off("Delete Slide", updatePresentationHandler);
@@ -56,7 +78,20 @@ const Provider = ({ children }) => {
 
 
 	return (
-		<AppContext.Provider value={{ activeUser, setActiveUser, presentation, setPresentation, currentSlide, setCurrentSlide, connectedUsers, setConnectedUsers }}>{children}</AppContext.Provider>
+		<AppContext.Provider
+			value={{
+				activeUser,
+				setActiveUser,
+				presentation,
+				setPresentation,
+				currentSlide,
+				setCurrentSlide,
+				connectedUsers,
+				setConnectedUsers,
+                viewers, editors, isUserViewer
+			}}>
+			{children}
+		</AppContext.Provider>
 	)
 }
 
