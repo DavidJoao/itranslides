5bb05076885ced6e784aefc42e6e8242996a0488
DavidJoao <davidsandoval596@gmail.com>
2024-11-29 19:20:19 -0800

    ADDED Reconnection Attempts and reconnectionDelay

--- src/app/lib/socket.js
+++ src/app/lib/socket.js
@@ -4,4 +4,6 @@ import io from 'socket.io-client'
 export const socket = io('wss://itranslides-websocket.onrender.com',{
     transports: ['websockets', 'polling'],
     reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
 });
